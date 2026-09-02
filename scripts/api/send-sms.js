88c6bcc43a2345da0275f2135c79568e8319ae1b
George <georgesakel8@gmail.com>
2026-09-03 01:15:37 +0300

    fix code

--- scripts/api/send-sms.js
+++ scripts/api/send-sms.js
@@ -1,26 +1,26 @@
 import twilio from 'twilio';
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Only POST requests allowed' });
-  }
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Only POST requests allowed' });
+    }
 
-  const { phone, code } = req.body;
+    const { phone, code } = req.body;
 
-  const accountSid = process.env.TWILIO_ACCOUNT_SID;
-  const authToken = process.env.TWILIO_AUTH_TOKEN;
-  
-  const client = twilio(accountSid, authToken);
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-  try {
-    const message = await client.messages.create({
-      body: `Ο κωδικός ελέγχου εγγράφου gov.gr wallet είναι: ${code}`,
-      from: '+306945291569',
-      to: phone
-    });
+    const client = twilio(accountSid, authToken);
 
-    res.status(200).json({ success: true, messageId: message.sid });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
+    try {
+        const message = await client.messages.create({
+            body: `O kodikos elegxou egrammatos einai: ${code}`,
+            from: '+306945291569',
+            to: phone
+        });
+
+        res.status(200).json({ success: true, messageId: message.sid });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
 }
